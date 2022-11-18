659df8f0016aed2033ef93600bdf885ab10620a3
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2022-11-18 10:54:25 +0100

    init commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+npx sass ./src/sass/style.scss ./public/style.css
+cp ./src/index.html ./public/
+cp ./src/favicons/*.* ./public/
+cp -R ./src/images ./public/
+cp -R ./src/fonts ./public/fonts
+npx esbuild ./src/js/main.js --bundle --outfile=./public/main.js  --minify
+
